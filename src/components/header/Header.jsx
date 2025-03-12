56eaf5e7c198946e28a805a9b173c01094b96370
Fabricio Asat <fabricioasat01@gmail.com>
2025-03-12 20:28:09 -0300

    feat: desktop heaeder anim added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { motion } from "motion/react";
+
 import { Logo } from "./Logo";
 import { Hamburger } from "./Hamburger";
+import { Links } from "./Links";
 
 export const Header = () => {
   return (
-    <header className="relative h-12 sm:h-14 lg:h-16 flex items-center justify-start py-2 px-5 gap-x-3">
-      <Hamburger />
-      <Logo />
+    <header className="w-full">
+      <motion.span
+        className="relative h-12 sm:h-14 lg:h-16 flex items-center justify-start py-2 px-5 gap-x-3 max-w-7xl mx-auto"
+        initial={{ opacity: 0 }} // inicio opacidad 0
+        animate={{ opacity: 1 }} // termina opacidad 1
+        transition={{ duration: 0.5 }}
+      >
+        <span className="md:hidden">
+          <Hamburger />
+        </span>
+
+        <Logo />
+        <Links />
+      </motion.span>
     </header>
   );
 };
--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { motion } from "motion/react";
 
-export const Links = ({ isMobile = true }) => {
-  return <nav></nav>;
+export const Links = () => {
+  const menuItems = ["Inicio", "Sobre nosotros", "Servicios", "Contactos"];
+
+  return (
+    <motion.nav
+      className="items-center justify-center w-full gap-x-7 hidden md:flex"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      {menuItems.map((item, index) => (
+        <motion.li
+          key={index}
+          className="text-base text-black list-none italic font-bold hover:text-neutral-500"
+          initial={{ y: -20, opacity: 0 }} // Empieza movido hacia arriba
+          animate={{ y: 0, opacity: 1 }} // Vuelve a su posición
+          transition={{
+            duration: 0.5,
+            delay: index * 0.1, // Cada item aparece con un pequeño retraso
+          }}
+        >
+          {item}
+        </motion.li>
+      ))}
+    </motion.nav>
+  );
 };
