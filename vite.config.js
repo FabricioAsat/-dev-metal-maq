d488b206242cf7fd90a9af050f7254a7bf1c11b4
Fabricio Asat <fabricioasat00@gmail.com>
2025-03-18 21:58:53 -0300

    fix: fix routing again

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  server: {
-    historyApiFallback: true,
-  },
+  base: "/", // Asegura que las rutas sean relativas a la raíz
   plugins: [react(), tailwindcss()],
+  server: {
+    open: true, // Opcional: abre el navegador automáticamente al iniciar dev
+    port: 5173, // Opcional: el puerto donde corre Vite
+  },
+  build: {
+    outDir: "dist", // Carpeta donde se genera el build
+  },
 });
